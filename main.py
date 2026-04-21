1ce61a28f8a8d5b9e93b7d50044c4b2fa49a2767
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2026-04-21 22:14:27 +0200

    added my planning of the main file, and also the lis of dictionaries of the students

---
+++ main.py
@@ -0,0 +1,33 @@
+#1. CREATE A LIST OF DICTIONARIES WITH STUDENT INFO
+#2.CREATE THE FUNCTIONS THAT WILL PERFORM THE DIFFERENT Tasks
+#3. Within the functions, there has to be a way the functions are able to access the student system
+# and perform their tasks, with the required concepts
+
+#1. LIST OF DICTIONARIES
+
+students = [
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+    {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5}, {"name": "Lisa", "age": 26, "course": "Law", "marks": 76.5},
+]
+
+
+
+
+
+
+
+
+
+
+
