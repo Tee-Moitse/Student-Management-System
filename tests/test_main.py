0f39df1e030dcc8f9d7c2188362eceaa2336f177
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2026-06-14 20:05:36 +0200

    writtent the tests templates in the test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,31 @@
 #unit testing for the functions using setup for the database
 #and that the different test cases dont affect database in each function test.
 
+from tests import test_main
+import unittest
 
+class TestStudentSystem(unittest.TestCase):
+    
+    
+    def setUp(self):
+        pass
+    
+    def test_something(self):
+        pass
+    
+    def test_something(self):
+        pass
+    
+    def test_something(self):
+        pass
+    
+    def test_something(self):
+        pass
+    
+    def test_something(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    unittest.main()
+    
